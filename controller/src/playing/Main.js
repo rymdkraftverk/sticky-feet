be8fea3b45b4045aa178e8ed6b71d611d5086bc4
Lorentz Lasson <lasson.lorentz@gmail.com>
2019-07-15 20:19:33 +0200

    Jump button

--- controller/src/playing/Main.js
+++ controller/src/playing/Main.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Event } from 'common'
+import styled from 'styled-components/macro'
+import Div100vh from 'react-div-100vh'
+
+const Container = styled(Div100vh)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Button = styled.div`
+  border: 0.1em solid;
+  font-size: 8vw;
+`
 
 const GamePlaying = ({ send }) => {
   return (
-    <div onMouseDown={() => send({ event: Event.ToGame.JUMP })}>
-      Game Playing!
-    </div>
+    <Container>
+      <Button onMouseDown={() => send({ event: Event.ToGame.JUMP })}>
+        Jump
+      </Button>
+    </Container>
   )
 }
 
