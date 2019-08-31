0ba56bc3a46e95baa70668d7647ff3429956cda0
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-08-31 12:32:07 +0200

    Style join instructions

--- game/src/game/stage.js
+++ game/src/game/stage.js
@@ -66,12 +66,48 @@ const createDome = () => {
   Matter.World.add(state.matterWorld, [marker])
 }
 
-const TEXT_X = 12
-const URL_Y = 50
-const CODE_Y = 150
+const drawInstructionArrow = ({
+  x, y, angle,
+}) => {
+  const instructionArrow = new PIXI.Sprite(l1.getTexture('expand-arrow-one'))
+
+  instructionArrow.alpha = 0.2
+  instructionArrow.x = x
+  instructionArrow.y = y
+  instructionArrow.angle = angle
+
+  instructionArrow.cacheAsBitmap = true
+
+  return instructionArrow
+}
+
+const TEXT_X = 15
+const ARROW_X = TEXT_X + 150
+const PHONE_Y = 50
+const URL_Y = 155
+const CODE_Y = 300
+
+const BACKGROUND_WIDTH = 270
 
 const createJoinInstructions = (gameCode) => {
-  const urlLabel = new PIXI.Text('Url', { ...textStyle, fill: '#aaaaaa' })
+  const background = new PIXI.Graphics()
+  background.beginFill(l1.fromHex('#111111')).drawRect(0, 0, BACKGROUND_WIDTH, GAME_HEIGHT)
+  state.pixiStage.addChild(background)
+
+  const grabYourPhone = new PIXI.Text('Grab your phone', { ...textStyle, fill: '#aaaaaa', fontSize: 20 })
+  // @ts-ignore
+  grabYourPhone.position = { x: TEXT_X, y: PHONE_Y }
+  l1.makeResizable(grabYourPhone)
+  state.pixiStage.addChild(grabYourPhone)
+
+  const arrow1 = drawInstructionArrow({
+    x: ARROW_X,
+    y: PHONE_Y + 15,
+    angle: 90,
+  })
+  state.pixiStage.addChild(arrow1)
+
+  const urlLabel = new PIXI.Text('Go to', { ...textStyle, fill: '#aaaaaa' })
   // @ts-ignore
   urlLabel.position = { x: TEXT_X, y: URL_Y }
   l1.makeResizable(urlLabel)
@@ -79,9 +115,16 @@ const createJoinInstructions = (gameCode) => {
 
   const url = new PIXI.Text('www.fightgame.com', { ...textStyle, fontFamily: 'Arial' })
   // @ts-ignore
-  url.position = { x: TEXT_X, y: URL_Y + 26 }
+  url.position = { x: TEXT_X, y: URL_Y + 30 }
   l1.makeResizable(url)
   state.pixiStage.addChild(url)
+
+  const arrow2 = drawInstructionArrow({
+    x: ARROW_X,
+    y: URL_Y + 58,
+    angle: 90,
+  })
+  state.pixiStage.addChild(arrow2)
 
   const codeLabel = new PIXI.Text('Code', { ...textStyle, fill: '#aaaaaa' })
   // @ts-ignore
@@ -89,7 +132,9 @@ const createJoinInstructions = (gameCode) => {
   l1.makeResizable(codeLabel)
   state.pixiStage.addChild(codeLabel)
 
-  const code = new PIXI.Text(gameCode, { ...textStyle, fontFamily: 'Arial', fontSize: 64 })
+  const code = new PIXI.Text(gameCode, {
+    ...textStyle, fontFamily: 'Arial', fontSize: 64, fill: '#ff0000',
+  })
   // @ts-ignore
   code.position = { x: TEXT_X, y: CODE_Y + 26 }
   l1.makeResizable(code)
