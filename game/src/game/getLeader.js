abe482d39384a7a1fbd48e7b78edb2f4d4d45199
Axel Ulmestig <AxelUlmestig@users.noreply.github.com>
2019-09-06 07:36:37 +0200

    Fix issue where getLeader always returned the position that was behind (#68)

--- game/src/game/getLeader.js
+++ game/src/game/getLeader.js
@@ -1,36 +1,35 @@
 import {
-  DOME_X,
-  DOME_Y,
+  DOME_CENTER,
 } from './constant'
 
 import {
   subtract,
 } from './linearAlgebra'
 
-const DOME_CENTER = { x: DOME_X, y: DOME_Y }
-
 const getAngle = (p) => {
-  const { x: dx, y: dy } = subtract(p, DOME_CENTER)
+  const { x: dx, y: dy } = subtract(DOME_CENTER, p)
 
   return Math.atan2(dx, dy)
 }
 
+const modulus = (a, b) => (b + a) % a
+
+/*
+ * Y-axis goes from top to bottom and X-axis from left to right, so the angle
+ * of the polar coordinates will increase clockwise.
+ *
+ * The lizards run counter clockwise, so a greater angle means you're behind.
+ */
 const getLeader = (p1, p2) => {
-  const angle1 = getAngle(p1)
-  const angle2 = getAngle(p2)
+  const angleDiff = modulus(
+    2 * Math.PI,
+    getAngle(p2) - getAngle(p1),
+  )
 
-  const angleDiff = getAngle(p2) - getAngle(p1)
-  const differentAngleSign = (angle1 * angle2) < 0
-
-  if (differentAngleSign) {
-    return angleDiff > Math.PI
-      ? p2
-      : p1
-  }
-
-  return angleDiff > 0 && angleDiff < Math.PI
-    ? p1
-    : p2
+  // angle(p1) + anglediff (mod 2PI) = angle(p2) (mod 2PI)
+  return angleDiff < Math.PI
+    ? p2
+    : p1
 }
 
 export default getLeader
--- game/test/getLeader.test.js
+++ game/test/getLeader.test.js
@@ -1,6 +1,5 @@
 import {
-  DOME_X,
-  DOME_Y,
+  DOME_CENTER,
 } from '../src/game/constant'
 
 import {
@@ -10,83 +9,55 @@ import {
 
 import getLeader from '../src/game/getLeader'
 
-const DOME_CENTER = { x: DOME_X, y: DOME_Y }
 const radius = 10
 
 const generateNearbyPair = angle => ({
-  p1: add(DOME_CENTER, rotate(angle + 0.1, { x: radius, y: 0 })),
-  p2: add(DOME_CENTER, rotate(angle - 0.1, { x: radius, y: 0 })),
+  leader: add(
+    DOME_CENTER,
+    rotate(angle - 0.1, { x: radius, y: 0 }),
+  ),
+  behind: add(
+    DOME_CENTER,
+    rotate(angle + 0.1, { x: radius, y: 0 }),
+  ),
 })
 
 test('0 radians', () => {
-  const { p1, p2 } = generateNearbyPair(0)
+  const { leader, behind } = generateNearbyPair(0)
 
-  expect(getLeader(p1, p2))
-    .toEqual(p1)
+  expect(getLeader(leader, behind))
+    .toEqual(leader)
 
-  expect(getLeader(p2, p1))
-    .toEqual(p1)
+  expect(getLeader(behind, leader))
+    .toEqual(leader)
 })
 
 test('Pi / 2 radians', () => {
-  const { p1, p2 } = generateNearbyPair(Math.PI / 2)
+  const { leader, behind } = generateNearbyPair(Math.PI / 2)
 
-  expect(getLeader(p1, p2))
-    .toEqual(p1)
+  expect(getLeader(leader, behind))
+    .toEqual(leader)
 
-  expect(getLeader(p2, p1))
-    .toEqual(p1)
+  expect(getLeader(behind, leader))
+    .toEqual(leader)
 })
 
-// --- Read ---
 test('Pi radians', () => {
-  const { p1, p2 } = generateNearbyPair(Math.PI)
+  const { leader, behind } = generateNearbyPair(Math.PI)
 
-  expect(getLeader(p1, p2))
-    .toEqual(p1)
+  expect(getLeader(leader, behind))
+    .toEqual(leader)
 
-  expect(getLeader(p2, p1))
-    .toEqual(p1)
+  expect(getLeader(behind, leader))
+    .toEqual(leader)
 })
 
 test('3 Pi / 4 radians', () => {
-  const { p1, p2 } = generateNearbyPair(3 * Math.PI / 4)
+  const { leader, behind } = generateNearbyPair(3 * Math.PI / 4)
 
-  expect(getLeader(p1, p2))
-    .toEqual(p1)
+  expect(getLeader(leader, behind))
+    .toEqual(leader)
 
-  expect(getLeader(p2, p1))
-    .toEqual(p1)
+  expect(getLeader(behind, leader))
+    .toEqual(leader)
 })
-
-/*
-test('find', () => {
-  expect(repo.find('foo'))
-    .toEqual(PLAYERS[0])
-})
-
-// --- Write ---
-
-test('add', () => {
-  repo.add({
-    id: 'unique',
-    color: 'yellow',
-  })
-
-  expect(state.players)
-    .toEqual(PLAYERS.concat({
-      id: 'unique',
-      color: 'yellow',
-    }))
-})
-
-test('remove', () => {
-  repo.remove('bar')
-
-  expect(state.players)
-    .toEqual([{
-      id: 'foo',
-      color: 'blue',
-    }])
-})
-*/
